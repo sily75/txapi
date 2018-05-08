06e2c95ba6c682930722da6f9afb9a484d7a8ec2
sily75 <lilinghua401@163.com>
2018-05-08 10:09:54 +0800

    Update md5sign.py

--- md5sign.py
+++ md5sign.py
@@ -1,14 +1,3 @@
-"""
--------------------------------------------------
-   File Name：     md5sign
-   Description :
-   Author :       LingHua
-   date：          2017/2/8
--------------------------------------------------
-   Change Activity:
-                   2018/2/9:
--------------------------------------------------
-"""
 import hashlib
 import time
 import random
@@ -24,14 +13,11 @@ def get_params(plus_item):
     '''请求时间戳（秒级），用于防止请求重放（保证签名5分钟有效）'''
     t = time.time()
     time_stamp=str(int(t))
-
     '''请求随机字符串，用于保证签名不可预测'''
     nonce_str = ''.join(random.sample(string.ascii_letters + string.digits, 10))
-
     '''应用标志，这里修改成自己的id和key'''
-    app_id = '1106655015'
-    app_key = 'Wz0bsFWuNGGf58ln'
-
+    app_id = 'xxxxxxxxxxx'
+    app_key = 'xxxxxxxxxxxx'
     params = {'app_id':app_id,
                  'question':plus_item,
                  'time_stamp':time_stamp,
@@ -39,7 +25,6 @@ def get_params(plus_item):
                  'sign':'',
                  'session':'10000'
              }
-
     sign_before = ''
     for key in sorted(params):
         if params[key] != '':
@@ -47,12 +32,10 @@ def get_params(plus_item):
             sign_before += key + '=' + quote(params[key]) + '&'
     # 将应用密钥以app_key为键名，拼接到字符串sign_before末尾
     sign_before += 'app_key=' + app_key
-
     # 对字符串sign_before进行MD5运算，将得到的MD5值所有字符转换成大写，得到接口请求签名
     sign=curlmd5(sign_before)
     sign=sign.upper()
     params['sign'] = sign
-
     return params
 
 
