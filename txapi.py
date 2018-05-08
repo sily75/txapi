7b0a34404da1d9a31f71981964c79b669c457fa3
sily75 <lilinghua401@163.com>
2018-05-08 10:11:02 +0800

    Update txapi.py

--- txapi.py
+++ txapi.py
@@ -14,9 +14,6 @@ if __name__ == '__main__':
         comment = input('我：')
         if comment == 'q':
             break
-        #不能有空白符，并且只能识别200字节。comment不转utf-8也可以。
         comment = re.sub(r'\s','',comment)
-        # text,polar,confd=get_content(comment)
         answer=get_content(comment)
-        # print('文本：'+text+'\n'+'情感倾向：'+str(polar)+'\n'+'程度：'+str(confd))
         print('机器人：，'+answer)
