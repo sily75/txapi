cf0595ad15c5e886982d064239fb0839803de23a
sily75 <lilinghua401@163.com>
2018-05-11 15:29:20 +0800

    Update txapi.py

--- txapi.py
+++ txapi.py
@@ -5,6 +5,7 @@ import re
 def get_content(plus_item):
     # url = "https://api.ai.qq.com/fcgi-bin/nlp/nlp_textpolar"  # 情感分析的API地址
     url = "https://api.ai.qq.com/fcgi-bin/nlp/nlp_textchat"  # 聊天的API地址
+    plus_item = plus_item.encode('utf-8')
     payload = md5sign.get_params(plus_item)#获取请求参数
     r = requests.get(url,params=payload)
     return r.json()["data"]["answer"]
@@ -14,6 +15,5 @@ if __name__ == '__main__':
         comment = input('我：')
         if comment == 'q':
             break
-        comment = re.sub(r'\s','',comment)
         answer=get_content(comment)
         print('机器人：，'+answer)
